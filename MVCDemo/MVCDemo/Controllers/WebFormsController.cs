b4a70bc7752278cd2b1dc36101a7b8295dd9037d
linxunke <linxunke1@gmail.com>
2015-11-13 20:50:32 +0000

    bao

---
+++ MVCDemo/MVCDemo/Controllers/WebFormsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class WebFormsController : Controller
+    {
+        //
+        // GET: /WebForms/
+        public ActionResult Index()
+        {
+            return View("WebForm1");
+        }
+        public ActionResult WebForm1()
+        {
+            return View();
+        }
+	}
+}
